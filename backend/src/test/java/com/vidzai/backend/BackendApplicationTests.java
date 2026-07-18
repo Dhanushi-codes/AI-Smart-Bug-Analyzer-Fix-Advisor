202077dc8337ae269d18468a07e9c2b37c029cb9
Dhanushi <dhanushibsccs@gmail.com>
2026-07-18 19:12:58 +0530

    Set up Spring Boot Backend

---
+++ backend/src/test/java/com/vidzai/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidzai.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
